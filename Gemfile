7f85509ef5681e82d0ed349497e24da803ba0afc
wangan2293 <mtktdpw@gmail.com>
2018-10-10 20:49:53 +0900

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.4.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
+gem 'mysql2', '>= 0.4.4','<0.6.0'
 # Use sqlite3 as the database for Active Record
 gem 'bcrypt'
 gem 'bootstrap-sass', '3.3.7'
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2018_10_09_132024) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
